347d765d80b1666e8c230d3106e68cd9a7282449
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2021-05-21 17:48:26 +0530

    removed warnings

--- src/constant/routes.js
+++ src/constant/routes.js
@@ -2,6 +2,6 @@ const HOME = "/";
 const SIGN_IN = "/signin";
 const SIGN_UP = "/signup";
 const DASHBOARD = "/:userId/dashboard";
-const NOTEBOOKS = "/:userId/notebooks";
+// const NOTEBOOKS = "/:userId/notebooks";
 
 export { HOME, SIGN_IN, SIGN_UP, DASHBOARD };

96ee3c8b63629c19b6db3873e38c825c3f89f148
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2021-07-03 21:40:27 +0530

    updated notesSlice loading indicator to show initially only

--- src/features/notesSlice.js
+++ src/features/notesSlice.js
@@ -1,80 +1,98 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { firestore, timeStamp } from "../firebase";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { auth, firestore, timeStamp } from "../firebase";
 
-export const getAllNotesAsync = (userId, type) => {
-  return async (dispatch) => {
-    let getTrashNotes = false;
-    if (type === "notes") {
-      getTrashNotes = false;
+export const getAllNotesAsync = createAsyncThunk(
+  "notes/getAllNotes",
+  async ({ type }) => {
+    const uid = auth?.currentUser?.uid;
+
+    let includeTrashNotes = false;
+    if (type === "GET_NOTES") {
+      includeTrashNotes = false;
     } else {
-      getTrashNotes = true;
+      includeTrashNotes = true;
     }
     const res = await firestore
       .collection("notes")
-      .where("moveToTrash", "==", getTrashNotes)
-      .where("user", "==", firestore.collection("users").doc(userId))
+      .where("moveToTrash", "==", includeTrashNotes)
+      .where("user", "==", firestore.collection("users").doc(uid))
       .orderBy("lastEdited", "desc")
       .get();
     const docs = res.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    dispatch(setNotes({ docs }));
-  };
-};
 
-export const createNoteAsync = ({ title, content, userId }) => {
-  return async () => {
+    return docs;
+  }
+);
+
+export const createNoteAsync = createAsyncThunk(
+  "notes/createNote",
+  async ({ title, content }) => {
+    const { uid } = auth?.currentUser;
+
     const doc = await firestore.collection("notes").add({
       title: title || "Empty note",
       content: content,
-      user: firestore.collection("users").doc(userId),
+      user: firestore.collection("users").doc(uid),
       moveToTrash: false,
       lastEdited: timeStamp(),
       createdAt: timeStamp(),
     });
 
     return doc.id;
-  };
+  }
+);
+
+export const moveNoteToTrashAsync = (noteId) => async (dispatch) => {
+  await firestore.collection("notes").doc(noteId).update({
+    moveToTrash: true,
+  });
+
+  dispatch(getAllNotesAsync({ type: "GET_NOTES" }));
 };
 
-export const moveNoteToTrashAsync = (noteId, userId) => {
-  return async (dispatch) => {
+export const updateNoteAsync = createAsyncThunk(
+  "notes/updateNote",
+  async ({ noteId, _text, _title }) => {
     await firestore.collection("notes").doc(noteId).update({
-      moveToTrash: true,
-    });
-    dispatch(getAllNotesAsync(userId, "notes"));
-  };
-};
-
-export const updateNoteAsync = (noteId, title, text) => {
-  return async () => {
-    await firestore.collection("notes").doc(noteId).update({
-      title: title,
-      content: text,
+      title: _title,
+      content: _text,
+      moveToTrash: false,
       lastEdited: timeStamp(),
     });
-  };
-};
+  }
+);
 
-export const deleteNoteAsync = (noteId, userId) => {
+export const deleteNoteAsync = (noteId) => {
   return async (dispatch) => {
     await firestore.collection("notes").doc(noteId).delete();
-    dispatch(getAllNotesAsync(userId, "trash"));
+    dispatch(getAllNotesAsync());
   };
 };
 const notesSlice = createSlice({
   name: "notes",
   initialState: {
     notes: [],
+    loading: true,
   },
   reducers: {
-    setNotes: (state, action) => {
-      state.notes = [...action.payload.docs];
-    },
     setNotesEmpty: (state) => {
       state.notes = [];
+    },
+  },
+  extraReducers: {
+    [getAllNotesAsync.rejected]: (state, action) => {
+      // console.log(action);
+    },
+    [getAllNotesAsync.pending]: (state) => {
+      if (state.notes.length < 0) {
+        state.loading = true;
+      }
+    },
+    [getAllNotesAsync.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.notes = [...action.payload];
     },
   },
 });
 export const { setNotes, setNotesEmpty } = notesSlice.actions;
 export default notesSlice.reducer;
-
-export const selectAllNotes = (state) => state.notes.notes;

143bd990cc616d357822eadb035246f679622858
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2021-11-16 12:26:51 +0530

    add & update: SignIn, SignIn/Loadable

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,12 @@
 import { makeStyles, TextField } from '@material-ui/core';
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { EmailOutlined, LockOutlined } from '@material-ui/icons';
 import ButtonSubmitting from '../../components/ButtonSubmitting';
 import routes from '../../constant/routes';
 import strings from '../../constant/strings';
-import { signinWithCredentials } from '../../services';
+import { requestSignIn } from '../../store/slices/auth';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -52,19 +53,22 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function SignIn() {
-  const classes = useStyles();
   const [input, setInput] = useState({ email: '', password: '' });
-  const [error, setError] = useState('');
-  const [submitting, setSubmitting] = useState(false);
+  const dispatch = useDispatch();
   const history = useHistory();
+  const classes = useStyles();
 
-  const setSignIn = async (e) => {
+  const { loading, error } = useSelector((state) => state.auth);
+
+  const handleSignIn = async (e) => {
     e.preventDefault();
 
-    setSubmitting(true);
+    if (!input.email || !input.password) {
+      return;
+    }
     const { email, password } = input;
     try {
-      const res = await signinWithCredentials(email, password);
+      const res = await dispatch(requestSignIn({ email, password })).unwrap();
       if (res?.user) {
         history.push(routes.notes.route);
       }
@@ -73,9 +77,12 @@ function SignIn() {
         ...input,
         password: '',
       });
-      setError(err?.message || strings.SOMETHING_WENT_WRONG);
-      setSubmitting(false);
     }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
   };
 
   return (
@@ -91,8 +98,8 @@ function SignIn() {
             placeholder="Email"
             aria-label="Email"
             variant="filled"
+            onChange={handleChange}
             value={input.email}
-            onChange={({ target }) => setInput({ ...input, [target.name]: target.value })}
             InputProps={{
               disableUnderline: true,
               startAdornment: <EmailOutlined />,
@@ -103,25 +110,20 @@ function SignIn() {
             name="password"
             placeholder="Password"
             aria-label="Password"
-            value={input.password}
             variant="filled"
-            onChange={({ target }) => setInput({ ...input, [target.name]: target.value })}
+            value={input.password}
+            onChange={handleChange}
             InputProps={{
               disableUnderline: true,
               startAdornment: <LockOutlined />,
             }}
           />
-          <ButtonSubmitting
-            submitting={submitting}
-            submit={setSignIn}
-            disabled={input.email === '' || input.password === ''}
-            btnText={strings.SIGN_IN}
-          />
+          <ButtonSubmitting submitting={loading} submit={handleSignIn} btnText={strings.SIGN_IN} />
         </form>
         {error && <p className="error">{error}</p>}
         <p>
           Don&apos;t have an account?{' '}
-          <Link to={routes.signUp.route} className={classes.link}>
+          <Link to={routes.signup.route} className={classes.link}>
             {strings.SIGN_UP}
           </Link>
         </p>
